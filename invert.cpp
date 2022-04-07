ef52556dcff1473909b7886d6611cfd2c06d7ce1
YOUSSEFEYATE <youssef472003@gmail.com>
2022-04-07 16:01:03 +0200

    invert

---
+++ invert.cpp
@@ -0,0 +1,87 @@
+// Program: demo2.cpp
+// Purpose: Demonstrate use of bmplip for handling
+//          bmp colored and grayscale images
+//          Program load a gray image and store in another file
+// Author:  Mohammad El-Ramly
+// Date:    30 March 2018
+// Version: 1.0
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cmath>
+#include "bmplib.cpp"
+int z,y;
+
+using namespace std;
+unsigned char image[SIZE][SIZE];
+
+void loadImage ();
+void saveImage ();
+void doSomethingForImage ();
+
+int main()
+{
+  loadImage();
+  doSomethingForImage();
+  saveImage();
+  return 0;
+}
+
+//_________________________________________
+void loadImage () {
+   char imageFileName[100];
+
+   // Get gray scale image file name
+   cout << "Enter the source image file name: ";
+   cin >> imageFileName;
+
+   // Add to it .bmp extension and load image
+   strcat (imageFileName, ".bmp");
+   readGSBMP(imageFileName, image);
+}
+
+//_________________________________________
+void saveImage () {
+   char imageFileName[100];
+
+   // Get gray scale image target file name
+   cout << "Enter the target image file name: ";
+   cin >> imageFileName;
+
+   // Add to it .bmp extension and load image
+   strcat (imageFileName, ".bmp");
+   writeGSBMP(imageFileName, image);
+}
+
+//_________________________________________
+void doSomethingForImage() {
+  for (int i = 0; i < SIZE; i++) {
+    for (int j = 0; j< SIZE; j++) {
+
+// Example code to convert to BW the image
+   //A better version should NOt use 127 but the
+   //average of the pixels
+
+z = image[i][j];
+if (z <127 )
+{
+   y = 127 -  z;
+   y +=127 ;
+}
+
+else if (z >127 )
+{
+   y = z - 127;
+   y = 127 - y;
+}
+else if (z==127){
+
+y = 127;
+}
+image[i][j]=y;
+
+// do something with the image
+    }
+  }
+}
